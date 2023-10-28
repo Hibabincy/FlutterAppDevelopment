789ff784cd1457d9499041f473ffb1f2e54b4dcf
HIBA BINCY C <hibabincyc@gmail.com>
2023-10-28 15:27:02 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MaterialApp(home: Center(child: Text("Hello Everyone ",style :TextStyle(fontSize: 80,decoration: TextDecoration.none,letterSpacing: 2.0,fontStyle: FontStyle.italic ,color :Colors.blue,backgroundColor: Colors.black)))));
+
+}
